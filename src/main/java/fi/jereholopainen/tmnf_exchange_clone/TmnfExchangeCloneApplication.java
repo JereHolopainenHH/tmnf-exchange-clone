572e8907e1d319c2fdb26a6f65c241efb038d6d6
Jere Holopainen <jere.holopainen@myy.haaga-helia.fi>
2024-10-29 09:18:17 +0200

    Initial commit

---
+++ src/main/java/fi/jereholopainen/tmnf_exchange_clone/TmnfExchangeCloneApplication.java
@@ -0,0 +1,13 @@
+package fi.jereholopainen.tmnf_exchange_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmnfExchangeCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmnfExchangeCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fi/jereholopainen/tmnf_exchange_clone/TmnfExchangeCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.jereholopainen.tmnf_exchange_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmnfExchangeCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
